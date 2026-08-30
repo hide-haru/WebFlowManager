14ddb08a3ac138d87b641293e15f5fe8a866cae6
hideharu <hideharusawada9@gmail.com>
2026-08-30 20:10:04 +0900

    Allow production frontend CORS

--- backend/src/main/java/com/example/sukemage/config/SecurityConfig.java
+++ backend/src/main/java/com/example/sukemage/config/SecurityConfig.java
@@ -43,7 +43,9 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-            List.of("http://localhost:5173")
+            List.of("http://localhost:5173",
+            		"https://webflowmanager-frontend.onrender.com"
+            )
         );
 
         configuration.setAllowedMethods(
